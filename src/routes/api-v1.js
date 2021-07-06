f720d56ee5ea179ebab737be2471497d22326c94
AlvinJK <ajk.alvin@gmail.com>
2021-07-06 21:10:26 +0700

    Create Windfinder

--- src/routes/api-v1.js
+++ src/routes/api-v1.js
@@ -3,13 +3,66 @@ const turf = require('@turf/turf');
 
 const processRegionRequest = require('../services/processRegionRequest');
 const getArchivedData = require('../services/getArchivedData');
-const sliceGribByRegion = require('../utils/sliceGribByRegion');
+const createWindfinderWind = require('../services/createWindfinderWind');
 
 var router = express.Router();
 
 router.get('/test', async function (request, response) {
+  // ReadyAbout meta data table: f401aa73-c0be-4acd-835c-60952484fe49
+  // {"type":"Polygon","coordinates":[[[5.720305,53.008259],[5.772853,53.008259],[5.772853,53.034974],[5.720305,53.034974],[5.720305,53.008259]]]}
+  let roi = {
+    type: 'Feature',
+    properties: {},
+    geometry: {
+      type: 'Polygon',
+      coordinates: [
+        [
+          [5.720305, 53.008259],
+          [5.772853, 53.008259],
+          [5.772853, 53.034974],
+          [5.720305, 53.034974],
+          [5.720305, 53.008259],
+        ],
+      ],
+    },
+  };
+
+  // let bboxFromDB = {
+  //   type: 'Polygon',
+  //   coordinates: [
+  //     [
+  //       [4.731845, 52.026334],
+  //       [4.737645, 52.026334],
+  //       [4.737645, 52.03329],
+  //       [4.731845, 52.03329],
+  //       [4.731845, 52.026334],
+  //     ],
+  //   ],
+  // };
+  // let bbox = turf.bbox(bboxFromDB);
+  // const leftLon = bbox[0];
+  // const bottomLat = bbox[1];
+  // const rightLon = bbox[2];
+  // const topLat = bbox[3];
+  // console.log(bbox);
+  // [ -17.0068359375, 47.35371061951363, 2.5927734375, 58.367156332478885 ]
+  // https://api.windfinder.com/v2/maps/spots/boundingbox/?ne=58.367156332478885,2.5927734375&sw=47.35371061951363,-17.0068359375&z=7&customer=wfweb&version=1.0&token=43200b113a4566095f7be73a3c6c3f42
+  // https://api.windfinder.com/v2/maps/spots/boundingbox/?ne=${bbox[3]},${bbox[2]}&sw=${bbox[1]},${bbox[0]}&z=9&customer=wfweb&version=1.0&token=43200b113a4566095f7be73a3c6c3f42
+
+  // let poly = turf.bboxPolygon([
+  //   -5.839233398437501, 53.33415333620371, -0.5630493164062501, 55.637298574163,
+  // ]);
+  // let area = turf.area(poly);
+  // console.log(area);
+  // https://api.windfinder.com/v2/maps/spots/boundingbox/?ne=55.637298574163,-0.5630493164062501&sw=53.33415333620371,-5.839233398437501&z=9&customer=wfweb&version=1.0&token=dfd6008a22504910678655e303e27780
+  // console.log(
+  //   'url:',
+  //   `https://api.windfinder.com/v2/maps/spots/boundingbox/?ne=${bbox[3]},${bbox[2]}&sw=${bbox[1]},${bbox[0]}&z=9&customer=wfweb&version=1.0&token=43200b113a4566095f7be73a3c6c3f42`,
+  // );
+  const reports = await createWindfinderWind(roi);
   response.json({
     message: 'This is a test url',
+    reports,
   });
 });
 
---
+++ src/services/createWindfinderWind.js
@@ -0,0 +1,88 @@
+const puppeteer = require('puppeteer');
+const axios = require('axios');
+const turf = require('@turf/turf');
+
+async function getWindfinderToken() {
+  const browser = await puppeteer.launch({
+    headless: true,
+    args: ['--no-sandbox'],
+  });
+  const page = await browser.newPage();
+  // Using default 30000ms timeout often results in timeout exceeded error
+  page.setDefaultNavigationTimeout(90000);
+  await page.goto('https://windfinder.com');
+  const token = await page.evaluate(() => {
+    return API_TOKEN;
+  });
+  await browser.close();
+  return token;
+}
+
+async function getWindfinderWind(roi) {
+  const token = await getWindfinderToken();
+  const bbox = turf.bbox(roi);
+  const leftLon = bbox[0];
+  const bottomLat = bbox[1];
+  const rightLon = bbox[2];
+  const topLat = bbox[3];
+
+  const spotsUrl = `https://api.windfinder.com/v2/maps/spots/boundingbox/?ne=${topLat},${rightLon}&sw=${bottomLat},${leftLon}&z=9&customer=wfweb&version=1.0&token=${token}`;
+  const spotsResponse = await axios.get(spotsUrl);
+  const { data: spotsData } = spotsResponse;
+  const windfinderReports = [];
+  if (Array.isArray(spotsData)) {
+    for (let i = 0; i < spotsData.length; i++) {
+      const { id: spotID, tp, lat, lon, n: stationName } = spotsData[i];
+      //check if the tp is w (weather station)
+      if (tp === 'w') {
+        const pinPoint = turf.point([lon, lat]);
+        const isWithinRoi = turf.booleanWithin(pinPoint, roi);
+        if (isWithinRoi) {
+          // Do get the report from this spot
+          const dataUrl = `https://api.windfinder.com/v2/spots/${spotID}/reports/?limit=-1&timespan=last24h&step=1m&customer=wfweb&version=1.0&token=${token}`;
+          const reportData = await axios.get(dataUrl);
+          const reports = [];
+          reportData.data.forEach((datum) => {
+            const windSpeedKTS = datum.ws;
+            const windGustKTS = datum.wg;
+            const windDirectionDegrees = datum.wd;
+            const atmosphericPressureMB = datum.ap;
+            // TODO: what are these two times?
+            const time1 = datum.dtl;
+            const time2 = datum.dtl_s;
+
+            // "ws":8,"wd":90,"at":29.0,"ap":927,"cl":25,"dtl":"2021-07-05T13:00:00+07:00","dtl_s":"2021-07-05T12:59:30+07:00"
+
+            reports.push({
+              windSpeedKTS,
+              windGustKTS,
+              windDirectionDegrees,
+              atmosphericPressureMB,
+              time1,
+              time2,
+            });
+          });
+          windfinderReports.push({
+            spotID,
+            stationName,
+            lon,
+            lat,
+            reports,
+          });
+        }
+      }
+    }
+  }
+  // {"id":"gb1069","n":"Shanes Hill","lat":54.82,"lon":-5.925,"kw":"shanes_hill","tp":"f","has":"1001010"}
+  //https://api.windfinder.com/v2/spots/id209/reports/?limit=-1&timespan=2021-07-05T00%3A00%3A00%2B07%3A00%2FPT23H59M59S&step=1m&customer=wfweb&version=1.0&token=43200b113a4566095f7be73a3c6c3f42
+  //https://api.windfinder.com/v2/spots/id209/reports/?limit=-1&timespan=last24h&step=1m&customer=wfweb&version=1.0&token=dfd6008a22504910678655e303e27780
+
+  return windfinderReports;
+}
+
+const createWindfinderWind = async (roi) => {
+  const data = await getWindfinderWind(roi);
+  return data;
+};
+
+module.exports = createWindfinderWind;
