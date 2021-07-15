4117acb72778018ab72b53f86112e5b00d702715
AlvinJK <ajk.alvin@gmail.com>
2021-07-15 17:42:06 +0700

    Added env example, separated upload s3 to another bucket

--- src/utils/uploadStreamToS3.js
+++ src/utils/uploadStreamToS3.js
@@ -5,7 +5,7 @@ const s3 = new AWS.S3({
   accessKeyId: process.env.AWS_S3_ACCESS_KEY_ID,
   secretAccessKey: process.env.AWS_S3_SECRET_ACCESS_KEY,
 });
-const bucketName = process.env.AWS_S3_BUCKET;
+const bucketName = process.env.AWS_S3_SLICED_BUCKET;
 
 function uploadStreamToS3(fileName) {
   var passThrough = new stream.PassThrough();
