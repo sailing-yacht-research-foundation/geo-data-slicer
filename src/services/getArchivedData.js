bec244cea3c19ef933883ddda1ad3f5161ebc875
AlvinJK <ajk.alvin@gmail.com>
2022-02-09 13:41:37 +0700

    Remove duplicate same file slice from uploaded to s3 and stored to DB

--- src/services/getArchivedData.js
+++ src/services/getArchivedData.js
@@ -2,6 +2,7 @@ const path = require('path');
 const fs = require('fs');
 const { v4: uuidv4 } = require('uuid');
 const turf = require('@turf/turf');
+const _ = require('lodash');
 
 const Queue = require('../classes/Queue');
 const logger = require('../logger');
@@ -11,7 +12,10 @@ const downloadFromS3 = require('../utils/downloadFromS3');
 const sliceGribByRegion = require('../utils/sliceGribByRegion');
 const uploadStreamToS3 = require('../utils/uploadStreamToS3');
 const { VALID_TIMEFRAME } = require('../configs/sourceModel.config');
-const { MAX_AREA_CONCURRENT_RUN } = require('../configs/general.config');
+const {
+  MAX_AREA_CONCURRENT_RUN,
+  WEATHER_FILE_TYPES,
+} = require('../configs/general.config');
 
 const Op = db.Sequelize.Op;
 const bucketName = process.env.AWS_S3_BUCKET;
@@ -127,6 +131,7 @@ async function processFunction(data) {
     logger.error(`Error downloading grib: ${error.message}`);
     return [];
   }
+  const bboxString = bbox.join(',');
   const bboxPolygon = turf.bboxPolygon(bbox);
   const currentTime = new Date();
   const currentYear = String(currentTime.getUTCFullYear());
@@ -139,6 +144,12 @@ async function processFunction(data) {
     );
     return [];
   }
+
+  const existingSlices = await slicedWeatherDAL.findExistingSlices({
+    competitionUnitId: raceID,
+    originalFileId: id,
+  });
+
   const { slicedGribs, geoJsons, runtimes } = await sliceGribByRegion(
     bbox,
     downloadPath,
@@ -156,27 +167,46 @@ async function processFunction(data) {
     slicedGribs.map(async (slicedGrib) => {
       const { filePath, variables, levels } = slicedGrib;
       let gribDetail = null;
-      const gribStream = fs.createReadStream(filePath);
       const gribUuid = uuidv4();
-      try {
-        const {
-          writeStream: gribWriteStream,
-          uploadPromise: gribUploadPromise,
-        } = uploadStreamToS3(
-          `${model}/${currentYear}/${currentMonth}/${currentDate}/forecast/${variables.join(
-            '-',
-          )}/gribfile/${gribUuid}.grib2`,
-          slicedBucket,
+      const sameSlice = existingSlices.find((sliceRow) => {
+        if (
+          sliceRow.fileType !== WEATHER_FILE_TYPES.grib ||
+          !_.isEqual(levels, sliceRow.levels) ||
+          !_.isEqual(variables, sliceRow.variables) ||
+          bboxString !== turf.bbox(sliceRow.boundingBox).join(',')
+        ) {
+          return false;
+        }
+        return true;
+      });
+      if (!sameSlice) {
+        try {
+          const gribStream = fs.createReadStream(filePath);
+          const {
+            writeStream: gribWriteStream,
+            uploadPromise: gribUploadPromise,
+          } = uploadStreamToS3(
+            `${model}/${currentYear}/${currentMonth}/${currentDate}/forecast/${variables.join(
+              '-',
+            )}/gribfile/${gribUuid}.grib2`,
+            slicedBucket,
+          );
+          gribStream.on('open', function () {
+            gribStream.pipe(gribWriteStream);
+          });
+          gribDetail = await gribUploadPromise;
+        } catch (error) {
+          logger.error(`Error uploading grib: ${error.message}`);
+        }
+      } else {
+        logger.info(
+          `Skipped uploading a grib slice of ${id}. Level: ${levels.join(
+            ',',
+          )}, Var: ${variables.join(',')}`,
         );
-        gribStream.on('open', function () {
-          gribStream.pipe(gribWriteStream);
-        });
-        gribDetail = await gribUploadPromise;
-      } catch (error) {
-        logger.error(`Error uploading grib: ${error.message}`);
       }
 
-      fs.unlink(filePath, () => {
+      fs.unlink(filePath, (err) => {
         logger.info(`sliced grib of model ${model}-${id} has been deleted`);
       });
       return {
@@ -211,25 +241,48 @@ async function processFunction(data) {
       .map(async (json) => {
         const uuid = uuidv4();
         const { variables, time, level, filePath } = json;
-        const gsTimes = [`${time}+00`];
+        const gsTimes = [new Date(`${time}+00`)];
 
         const endTimeModifier = VALID_TIMEFRAME[model] || 3600000;
         const startTime = gsTimes[0];
         const startTimeInUnix = Date.parse(startTime);
-        const endTime = new Date(startTimeInUnix + endTimeModifier);
+        const endTimeInUnix = startTimeInUnix + endTimeModifier;
         let jsonDetail = null;
-        try {
-          const { writeStream, uploadPromise } = uploadStreamToS3(
-            `${model}/${currentYear}/${currentMonth}/${currentDate}/forecast/${variables.join(
-              '-',
-            )}/geojson/${uuid}.json`,
-            slicedBucket,
+
+        const sameSlice = existingSlices.find((sliceRow) => {
+          if (
+            sliceRow.fileType !== WEATHER_FILE_TYPES.json ||
+            startTimeInUnix !== Date.parse(sliceRow.startTime) ||
+            endTimeInUnix !== Date.parse(sliceRow.endTime) ||
+            !_.isEqual([level], sliceRow.levels) ||
+            !_.isEqual(variables, sliceRow.variables) ||
+            !_.isEqual(gsTimes, sliceRow.runtimes) ||
+            bboxString !== turf.bbox(sliceRow.boundingBox).join(',')
+          ) {
+            return false;
+          }
+          return true;
+        });
+        if (!sameSlice) {
+          try {
+            const { writeStream, uploadPromise } = uploadStreamToS3(
+              `${model}/${currentYear}/${currentMonth}/${currentDate}/forecast/${variables.join(
+                '-',
+              )}/geojson/${uuid}.json`,
+              slicedBucket,
+            );
+            const readStream = fs.createReadStream(filePath);
+            readStream.pipe(writeStream);
+            jsonDetail = await uploadPromise;
+          } catch (error) {
+            logger.error(`Error uploading geojson: ${error.message}`);
+          }
+        } else {
+          logger.info(
+            `Skipped uploading a json slice of ${id}. Level: ${level}, Time: ${time}, Var: ${variables.join(
+              ',',
+            )}`,
           );
-          const readStream = fs.createReadStream(filePath);
-          readStream.pipe(writeStream);
-          jsonDetail = await uploadPromise;
-        } catch (error) {
-          logger.error(`Error uploading geojson: ${error.message}`);
         }
 
         fs.unlink(filePath, () => {
@@ -239,7 +292,7 @@ async function processFunction(data) {
         return {
           uuid,
           startTime: startTimeInUnix,
-          endTime: endTime.toISOString(),
+          endTime: endTimeInUnix,
           s3Key: jsonDetail?.Key,
           levels: [level],
           runtimes: gsTimes,
@@ -253,7 +306,7 @@ async function processFunction(data) {
   const arrayData = [
     ...gribFiles
       .filter((row) => {
-        return row.s3Key !== null;
+        return row.s3Key != null;
       })
       .map((row) => {
         return {
@@ -262,17 +315,18 @@ async function processFunction(data) {
           startTime: start_time,
           endTime: end_time,
           s3Key: row.s3Key,
-          fileType: 'GRIB',
+          fileType: WEATHER_FILE_TYPES.grib,
           boundingBox: bboxPolygon.geometry,
           levels: row.levels,
           variables: row.variables,
           runtimes,
           competitionUnitId: raceID,
+          originalFileId: id,
         };
       }),
     ...successJsons
       .filter((row) => {
-        return row.s3Key !== null;
+        return row.s3Key != null;
       })
       .map((row) => {
         return {
@@ -281,20 +335,25 @@ async function processFunction(data) {
           startTime: row.startTime,
           endTime: row.endTime,
           s3Key: row.s3Key,
-          fileType: 'JSON',
+          fileType: WEATHER_FILE_TYPES.json,
           boundingBox: bboxPolygon.geometry,
           levels: row.levels,
           variables: row.variables,
           runtimes: row.runtimes,
           competitionUnitId: raceID,
+          originalFileId: id,
         };
       }),
   ];
-  try {
-    await slicedWeatherDAL.bulkInsert(arrayData);
-  } catch (error) {
-    logger.error(`Error saving metadata to DB: ${error.message}`);
+
+  if (arrayData.length > 0) {
+    try {
+      await slicedWeatherDAL.bulkInsert(arrayData);
+    } catch (error) {
+      logger.error(`Error saving metadata to DB: ${error.message}`);
+    }
   }
+
   return successJsons.map((row) => {
     return {
       key: row.key,
@@ -313,7 +372,6 @@ async function getArchivedData(bbox, startTime, endTime, raceID) {
   let maxConcurrentProcess = 3;
   if (turf.area(bboxPolygon) > MAX_AREA_CONCURRENT_RUN) {
     maxConcurrentProcess = 1;
-    console.log('maxConcurrent', maxConcurrentProcess, turf.area(bboxPolygon));
   }
   const queue = new Queue({
     maxConcurrentProcess,
